23512adeb9e4f702a59c79d362f640c60726a7a1
LauraCastellanos13 <lauracastellanosym@gmail.com>
2022-11-23 17:20:52 -0500

    feat:add to cart info styles and component revision

--- react/components/AddToCartInfo.tsx
+++ react/components/AddToCartInfo.tsx
@@ -8,6 +8,12 @@ import styles from './styles.css'
 const AddToCartInfo = ({ blockClass }: { blockClass: string }) => {
   const container = generateBlockClass(styles.container, blockClass)
   const container__item = generateBlockClass(styles.container__item, blockClass)
+  const container__total = generateBlockClass(styles.container__total, blockClass)
+  const container__image = generateBlockClass(styles.container__image, blockClass)
+  const container__information = generateBlockClass(styles.container__information, blockClass)
+  const container__title = generateBlockClass(styles.container__title, blockClass)
+
+
   const productInfo = useProduct();
   const { orderForm: {
     items,
@@ -23,24 +29,24 @@ const AddToCartInfo = ({ blockClass }: { blockClass: string }) => {
           console.log(item)
           return (
             <div key={index} className={container__item}>
-              <div>
+              <div className={container__image}>
                 <img src={item.imageUrls.at1x} />
               </div>
-              <div>
-                <p>{item.name}</p>
-                <p>{item.id}</p>
+              <div className={container__information}>
+                <p className={container__title}>{item.name}</p>
+                {/*<p>{item.id}</p>*/}
                 <p>${item.price / 100}</p>
-                <p>Cant: {item.quantity}</p>
+                <p>Cantidad: {item.quantity}</p>
               </div>
             </div>
           )
         })
       }
-      <div>
+      <div className={container__total}>
         <p> Tienes {items.length} productos en el carrito</p>
-        <p>Total: ${totalizers[0]?.value / 100}</p>
+        {/* <p>Total: ${totalizers[0]?.value / 100}</p> */}
       </div>
-      <ButtonGroup />
+      <ButtonGroup blockClass={blockClass} />
     </div>
   )
 }

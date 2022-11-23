51dd284b0f78f7c166fad2e0a4951a84c26ccbc9
LauraCastellanos13 <lauracastellanosym@gmail.com>
2022-11-23 00:06:09 -0500

    feat:adding custom components, blocks and interface add to cart info

---
+++ react/components/ButtonGroup.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ButtonGroup = () => {
+  return (
+    <>
+      <div>
+        <a href='/'>Check out</a>
+      </div>
+      <div>
+        <button>Continua</button>
+        <a href='/'> Ver carrito</a>
+      </div>
+    </>
+  )
+}
+
+export default ButtonGroup;
